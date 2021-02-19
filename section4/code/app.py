f3023b53216ad9ccceca37940e554451e82e8560
JongGurlHan <gojkhan13@gmail.com>
2021-02-19 20:13:08 +0900

    0219 commit

--- section4/code/app.py
+++ section4/code/app.py
@@ -1,15 +1,42 @@
 from flask import Flask
 from flask_restful import Resource, Api
-
+                            #Resource is just a thing that our API can return and create and things like that
 app = Flask(__name__)
 
 api = Api(app)
+    #Api is just going to allow us to very easily add these Resources to it 
 
-class Student(Resource):
+items =[]
+
+
+
+
+#Api works with resources and every resouce has to be a class
+#class Item inherits from from the class resource 
+class Item(Resource):  
+   # @app.route('/item/<string:name>')
     def get(self, name):
-        return {'student': name}
+       item = filter(lambda x: x['name'] == name, items) #we're going to go through each item, execute this function  
+                                                         #and see if the item's name x matches the name which is the parameter
+                                                         #And if it does, then we're going to return it
+       
+        # for item in items:
+        #     if item['name'] ==name:
+        #         return item
+        return {'item' : None}, 404
+    
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+     
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
  
-api.add_resource(Student, 'student/<string:name>') #http://127.0.0.1:5000/student/Rolf
-#This resouce that we've created, the student, now is gona be accessible via our API,
-
-
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+#tell our API, This resouce that we've created, the student, now is gonna be accessible via our API,
+# '/item/<string:name>': we're going to access students like so 
+#This name variable is going to go straight in to the parameter'name'
+app.run(port=5000, debug=True)
